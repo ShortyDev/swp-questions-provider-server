dd26c08e6dc37ced46c6242d400bec382ccbfe08
ShortyDev <shortydev8274@gmail.com>
2021-05-18 14:54:14 +0200

    Fixed CORS

--- app.js
+++ app.js
@@ -59,6 +59,7 @@ const getRandomQuestion = () => {
 }
 
 app.get("/new/random", (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*")
     res.send(JSON.stringify(getRandomQuestion()))
 })
 
